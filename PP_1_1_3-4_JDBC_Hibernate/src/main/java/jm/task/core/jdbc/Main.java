15a781e14b37086e0c2bf406fe07eeb3269a33b9
Rahman <45047592+rahman951@users.noreply.github.com>
2024-07-10 18:34:46 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.*;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        
+        UserDao userDao = new UserDaoHibernateImpl();
+        
+        userDao.createUsersTable();
+        
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+        
+        
+    }
+    
+}
